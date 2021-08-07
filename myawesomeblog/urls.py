92d3ce01302f9ae2017c83c1bbfa4db8a5f1c7bc
Your Name <you@example.commelnikalex2014@gmail.com>
2021-08-07 18:37:40 +0300

    eeettt3

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -20,5 +20,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('a/', a.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
